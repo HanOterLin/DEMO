0817470552ddcc48bf7b7ec1fdd75678b3ade4ed
tlin088 <lin.tony@pwc.com>
2017-06-29 17:12:47 +0800

    add test

--- test.js
+++ test.js
@@ -3,45 +3,126 @@ const uuid = require('uuid');
 
 const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/test');
 
-const demo = sequelize.define('business_sponsor_survey', {
-    lastName: {
-        type: Sequelize.UUID,
-        field: 'uuid',
-        defaultValue: uuid.v4(),
+const Customer = sequelize.define('customer', {
+
+    customer: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: true,
+        validate: {
+            isNumeric: true
+        }
     },
-    firstName: {
-        type: Sequelize.STRING,
-        field: 'firstName',
+    first_name: {
+        type: Sequelize.STRING(100),
+        validate: {
+            isAlphanumeric: true
+        }
+    },
+    last_name: Sequelize.STRING(100),
+    identity_code: {
+        type: Sequelize.STRING(20),
+        allowNull: true,
+        validate: {
+            isNumeric: true
+        }
+    },
+    note: Sequelize.STRING(1000),
+    birth_date: Sequelize.DATE,
+
+
+    created_by: Sequelize.INTEGER,
+    updated_by: Sequelize.INTEGER,
+
+    cst_type: Sequelize.INTEGER,
+    cst_state_type:  {
+        type: Sequelize.INTEGER,
     }
+
 }, {
-    tableName: 'business_sponsor_survey',
+    tableName: 'customer',
+
+    updatedAt: 'updated',
+    createdAt: 'created',
+    timestamps: true
+});
+
+const StateType = sequelize.define('StateType', {
+
+    cst_state_type: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        validate: {
+        }
+    },
+    name: Sequelize.STRING(100),
+}, {
+    tableName: 'cst_state_type',
     timestamps: false
 });
 
-demo.schema('public');
 
-demo.sync({force: false})
+Customer.belongsTo(StateType, {
+    foreignKey: 'cst_state_type',
+    as: 'state_type'
+});
+
+StateType.hasMany(Customer, {
+    foreignKey: 'cst_state_type'
+});
+
+
+Customer.findAll( {
+    include: [
+        { model: db.Address, as: 'addresses' },
+        { model: db.StateType, as: 'state_type' }
+    ]
+})
+    .success(function (customers) {
+        res.json(200, customers);
+    })
+    .fail(function (error) {
+        res.json(500, { msg: error });
+    });
+
+StateType.schema('public');
+
+Customer.schema('public');
+
+StateType.sync({force: true})
     .then(() => {
         // demo.create(
         //     {
-        //     firstName: 'ddd',
+        //     firstName: '',
         //     }
         // )
-        demo.update({
-            uuid: 'ddd',
-        }, {
-            where: {
-                firstName: ddd,
-            }
-        })
-            .then((res) => {
-                console;
-            })
-            .catch(err => {
-                console.dir(err);
-            });
+        //     .then((res) => {
+        //         console;
+        //     })
+        //     .catch(err => {
+        //         console.dir(err);
+        //     });
     })
     .catch(err => {
         console.dir(err);
     });
 
+Customer.sync({force: true})
+    .then(() => {
+        // demo.create(
+        //     {
+        //     firstName: '',
+        //     }
+        // )
+        //     .then((res) => {
+        //         console;
+        //     })
+        //     .catch(err => {
+        //         console.dir(err);
+        //     });
+    })
+    .catch(err => {
+        console.dir(err);
+    });
